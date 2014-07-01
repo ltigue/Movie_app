48ba6dce5f0e9544a57524d8574afbe98b62b704
ltigue <moneynloans@yahoo.com>
2014-07-01 14:19:25 -0400

    Created Class
    Created class and controller for program.

---
+++ Movie/Movie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieProg.MvcApplication" Language="C#" %>
---
+++ Movieapp1/Movieapp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movieapp1.Startup))]
+namespace Movieapp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
